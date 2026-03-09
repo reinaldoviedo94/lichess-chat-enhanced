d9d67b1b5bd48cb6e364269950535001c7c13099
Reinaldo O. Linares <reinaldoviedo94@gmail.com>
2026-03-09 16:32:43 -0400

    initial commit

---
+++ services/lichess_chat_enhanced/lichess_chat_enhanced/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for lichess_chat_enhanced project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lichess_chat_enhanced.settings')
+
+application = get_wsgi_application()
